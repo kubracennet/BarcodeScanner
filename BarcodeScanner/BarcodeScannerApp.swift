bef0d0a5d575dc49730c875ce702bd39b8f573e9
kubracennet <kubracennetyavasoglu@gmail.com>
2023-05-28 14:52:22 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Kübra Cennet Yavaşoğlu on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
